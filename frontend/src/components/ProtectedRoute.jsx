ce4234ec47b0453c18a65626762837bf9a884c6f
HalfTrack024 <andrewmurray731@gmail.com>
2024-04-03 09:40:53 -0400

    start progress front end dev

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import {Navigate} from "react-router-dom"
+import {jwtDecode} from "jwt-decode"
+import api from "../api"    
+import {REFRESH_TOKEN, ACCESS_TOKEN } from "../constants"
+import { useState } from "react"
+
+function ProtectedRoute({childern}) {
+    const [isAuthorized, setIsAuthorized] = useState(null)
+
+    const refreshToken = async() => {
+
+    }
+
+    
+}
